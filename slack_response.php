bcb954d8fb12405744047cdad9ce472113fe90c3
Terukazu <terukazu@senku.jp>
2015-11-27 14:40:22 +0900

    GoogleとwikiのURLを返すように。

--- slack_response.php
+++ slack_response.php
@@ -1,10 +1,20 @@
 <?php
 //レスポンスを送る
-$text = $_POST["text"];
+$text = $_POST['text'];
+$str = array_values(array_filter(preg_split('/[ 　]/u', $_POST['text'])));
+if ($str[1] == "wiki") {
+	$text = "https://ja.wikipedia.org/wiki/".rawurlencode($str[2]);
+}elseif ($str[1] == ggr) {
+	$text = "https://www.google.co.jp/search?q=".rawurlencode($str[2]);
+}else{
+	$text = "いまはwikiとggrしかないようです・・・　「ぱぱぱ　wiki ほにゃらら」「ぱぱぱ　ggr　ほにゃらら」	と入力くださいな";
+}
 
+
+//Slackに返す
 echo json_encode(
 	array(
-		'text' => "テキスト".$text."テキスト",
+		'text' => $text,
 		'username' => $username,
 		'icon_url' => $icon_url
 	)
